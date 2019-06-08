44ffaae8bb0cafc76db3a671f5e19d80d9f97e76
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-06-08 13:21:24 +0300

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/app'
+import 'firebase/database'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBH6wvHTL779JnXD8U3rVsLb2TE5uAFygI",
+    authDomain: "m-city-53916.firebaseapp.com",
+    databaseURL: "https://m-city-53916.firebaseio.com",
+    projectId: "m-city-53916",
+    storageBucket: "m-city-53916.appspot.com",
+    messagingSenderId: "1065194218227",
+    appId: "1:1065194218227:web:fe99594c317d707b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const firebaseDB = firebase.database()
+
+  firebaseDB.ref('matches').once('value').then((snapshot)=>{
+      console.log(snapshot.val())
+  })
